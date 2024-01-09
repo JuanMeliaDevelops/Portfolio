76bc5e6de48bb9bee6548d1b9ddf1a88bc6d92a8
Juan <meliaj@medios.gob.ar>
2024-01-09 15:43:44 -0300

    Section Trabajos terminado

--- js/main.js
+++ js/main.js
@@ -33,48 +33,60 @@ scrollDown.classList.remove("animate__animated", "animate__bounce");
 
 
 
-
-// Mostrar Categorias Work-cards
 document.addEventListener('DOMContentLoaded', function () {
-    const categories = document.querySelectorAll('.work-categories li');
-    const workCards = document.querySelectorAll('.work-container .work-cards');
+const categories = document.querySelectorAll('.work-categories li');
+const workCards = document.querySelectorAll('.work-container .work-cards');
 
-    // Función para mostrar work-cards de la categoría seleccionada
-    function showSelectedCategory(selectedCategory) {
-        workCards.forEach(function (card) {
-          const cardCategories = card.getAttribute('data-category').split(' ');
-      
-          if (cardCategories.includes(selectedCategory)) {
+// Función para mostrar work-cards de la categoría seleccionada
+function showSelectedCategory(selectedCategory) {
+    workCards.forEach(function (card) {
+        const cardCategories = card.getAttribute('data-category').split(' ');
+
+        if (cardCategories.includes(selectedCategory)) {
             card.classList.add('animate__bounceIn');
             card.style.display = 'block';
-            
-          } else {
+        } else {
             card.style.display = 'none';
             card.classList.remove('animate__bounceIn');
-          }
+        }
+    });
+}
+
+// Mostrar solo las work-cards 'destacadas' al cargar la página
+showSelectedCategory('destacado');
+
+// Escuchar clics en las categoríass
+categories.forEach(function (category) {
+    document.getElementById('categoryDestacados').classList.add('selected-category');
+
+    category.addEventListener('click', function () {
+        // Eliminar la clase 'selected' de todos los elementos li
+        categories.forEach(li => {
+            
+
+            li.classList.remove('selected-category');
         });
-      }
-      
 
+        // Agregar la clase 'selected' solo al elemento clickeado
 
-    // Mostrar las work-cards destacadas al cargar la página
-    showSelectedCategory('destacados');
+        document.getElementById('categoryDestacados').classList.remove('selected-category');
+        this.classList.add('selected-category');
 
-    // Escuchar clics en las categorías
-    categories.forEach(function (category) {
-        category.addEventListener('click', function () {
-            // Elimina clase a todos los li categories
-            categories.forEach(li => {
-                li.classList.remove('selected-category');
-            });
+        const selectedCategory = this.getAttribute('data-category');
 
-            // Agregar la clase 'selected' solo al elemento clickeado
-            this.classList.add('selected-category');
+        // Mostrar solo las work-cards correspondientes a la categoría seleccionada
+        showSelectedCategory(selectedCategory);
 
-            const selectedCategory = this.getAttribute('data-category');
-
-            // Mostrar solo las work-cards correspondientes a la categoría seleccionada
-            showSelectedCategory(selectedCategory);
+        // Reanudar la animación para todas las work-cards que coincidan con la nueva categoría
+        workCards.forEach(function (card) {
+            const cardCategories = card.getAttribute('data-category').split(' ');
+            if (cardCategories.includes(selectedCategory)) {
+                card.classList.remove('animate__bounceIn');
+                setTimeout(() => {
+                    card.classList.add('animate__bounceIn');
+                }, 100);
+            }
         });
     });
+});
 });
