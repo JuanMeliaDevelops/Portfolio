1fcc0c265d596fda1039c1fe872f71da288c1989
juanimelia <juanimeliusa@gmail.com>
2024-02-28 20:31:03 -0300

    Animacion y seleccion de categorias Buttons - Sobre Mi

--- js/cats-content.js
+++ js/cats-content.js
@@ -1,80 +1,57 @@
 
 
-// Work Cards 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const categories = document.querySelectorAll('.cats-categories li');
-    const workCards = document.querySelectorAll('.work-container .work-cards');
-    
-    // Función para mostrar work-cards de la categoría seleccionada
-    function showSelectedCategory(selectedCategory) {
+// Cats Content - Sobre Mi ----------------
+const categories = document.querySelectorAll('.sobreMi-cats button');
+const contentCard = document.querySelectorAll('.sobreMi-cats-container a');
+
+
+function showSelectedCategory(selectedCategory) {
+    contentCard.forEach(function (card) {
+        const categories = card.getAttribute('data-category').split(' ');
+
+        if (categories.includes(selectedCategory)) {
+            card.classList.add('animate__fadeIn');
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+            card.classList.remove('animate__fadeIn');
+        }
+    });
+}
+
+
+
+// Escuchar clics en las categoríass
+categories.forEach(function (category) {
+
+
+
+    category.addEventListener('click', function () {
+        categories.forEach(button => {
+
+            button.classList.remove('selected-button');
+        });
+
+        this.classList.add('selected-button');
+
+        const selectedCategory = this.getAttribute('data-category');
+
+        // Mostrar solo las work-cards correspondientes a la categoría seleccionada
+        showSelectedCategory(selectedCategory);
+
+        // Reanudar la animación para todas las cards que coincidan con la nueva categoría
         workCards.forEach(function (card) {
             const cardCategories = card.getAttribute('data-category').split(' ');
-    
             if (cardCategories.includes(selectedCategory)) {
-                card.classList.add('animate__bounceIn');
-                card.style.display = 'block';
-            } else {
-                card.style.display = 'none';
-                card.classList.remove('animate__bounceIn');
+                card.classList.remove('animate__fadeIn');
+                setTimeout(() => {
+                    card.classList.add('animate__fadeIn');
+                }, 100);
             }
         });
-    }
-    
-    // Mostrar solo las work-cards 'destacadas' al cargar la página
-    showSelectedCategory('destacado');
-    
-    // Escuchar clics en las categoríass
-    categories.forEach(function (category) {
-        document.getElementById('categoryDestacados').classList.add('selected-category');
-    
-        category.addEventListener('click', function () {
-            // Eliminar la clase 'selected' de todos los elementos li
-            categories.forEach(li => {
-                
-    
-                li.classList.remove('selected-category');
-            });
-    
-            // Agregar la clase 'selected' solo al elemento clickeado
-    
-            document.getElementById('categoryDestacados').classList.remove('selected-category');
-            this.classList.add('selected-category');
-    
-            const selectedCategory = this.getAttribute('data-category');
-    
-            // Mostrar solo las work-cards correspondientes a la categoría seleccionada
-            showSelectedCategory(selectedCategory);
-    
-            // Reanudar la animación para todas las work-cards que coincidan con la nueva categoría
-            workCards.forEach(function (card) {
-                const cardCategories = card.getAttribute('data-category').split(' ');
-                if (cardCategories.includes(selectedCategory)) {
-                    card.classList.remove('animate__bounceIn');
-                    setTimeout(() => {
-                        card.classList.add('animate__bounceIn');
-                    }, 100);
-                }       
-            });
-        });
     });
-    });
-    
-    
-    
-    // Función para mostrar work-cards de la categoría seleccionada
-    function showSelectedCategory(selectedCategory) {
-        workCards.forEach(function (card) {
-            const cardCategories = card.getAttribute('data-category').split(' ');
-    
-            if (cardCategories.includes(selectedCategory)) {
-                card.classList.add('animate__bounceIn');
-                card.style.display = 'block';
-            } else {
-                card.style.display = 'none';
-                card.classList.remove('animate__bounceIn');
-            }
-        });
-    }
-    
-    
+});
+
+
+
